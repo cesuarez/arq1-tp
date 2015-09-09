48383d505b3b4039937ea30efe4bd0c7b9b4f1c8
Cristian Suarez <cris.eze.sua@gmail.com>
2015-09-09 08:33:52 -0300

    Project first Migrations

--- backend/database/migrations/2015_09_09_025335_Event.php
+++ backend/database/migrations/2015_09_09_025335_Event.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Event extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
-            $table->timestamps();
+            $table->datetime('date');
+            $table->enum('privacy', ['private', 'public']);
+            $table->string('location');
+            $table->text('description');
+            $table->text('proposed_requirements');
         });
     }
 
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('events');
     }
 }
