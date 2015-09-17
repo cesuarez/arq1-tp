2a3bab1c891e7645aac6574ea27bc4a7005f5593
Cristian Suarez <cris.eze.sua@gmail.com>
2015-09-16 23:35:40 -0300

    Agregado un comentario menor

--- backend/app/Event.php
+++ backend/app/Event.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model {
 
+	// Cannot be changed
     protected $guarded = ['id'];
+
+    // Cannot be accessed
     protected $hidden = ['created_at', 'updated_at'];
 }
