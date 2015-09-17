e7e8fa0e3294b8cb342f2b4d9bd7e68328a24437
Cristian Suarez <cris.eze.sua@gmail.com>
2015-09-17 02:01:59 -0300

    Empezado el agregado de Mongo

--- backend/config/app.php
+++ backend/config/app.php
@@ -145,6 +145,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+        * External Libraries Providers
+        */
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
+
     ],
 
     /*
