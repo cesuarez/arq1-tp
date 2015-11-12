ce8191f60ed7d72394bb61b94ce384a185488f79
sawady <sawady.faso@gmail.com>
2015-11-12 14:02:50 +0000

    add comments controller

--- frontend/app/angular/controllers/event-details.js
+++ frontend/app/angular/controllers/event-details.js
@@ -38,37 +38,5 @@ angular.module('angularApp').controller('EventDetailsCtrl', function($scope, $st
             };
         });
     });
-    
-    $scope.newComment = function() {
-        $scope.comment = {
-            comment: '',
-            user_id: $scope.authUser.id,
-            event_id: $scope.event.id
-        };
-    };
-
-    $scope.comments = Event.comments({ id: $stateParams.id });
-    
-    $scope.sendComment = function() {
-        Event.comment($scope.comment, function(data) {
-            $scope.comments = data;
-            $scope.newComment();
-        });
-    };
-    
-    $scope.moreComments = function() {
-        if($scope.comments.next_page_url) {
-            Event.comments({ 
-                id: $scope.event.id, 
-                page: $scope.comments.current_page + 1 
-            }, function(data) {
-                var comments = $scope.comments.data;
-                var newComments = data;
-                newComments.data = comments.concat(newComments.data);
-                $scope.comments = newComments;
-            });
-        }
-    };
-
 
 });
